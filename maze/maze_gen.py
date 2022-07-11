c194b80b3282dbac86335f3b403450b4d54ab2e2
eti-du <92732299+eti-du@users.noreply.github.com>
2022-07-11 15:40:15 +0200

    comment the code

--- maze/maze_gen.py
+++ maze/maze_gen.py
@@ -4,14 +4,13 @@ class lab_gen:
     def __init__(self, w, h):
         self.width = w
         self.height = h
-
         self.maze = []
-
         self.main()
 
 
 
     def main(self):
+        #create the structure of the maze full of walls
         for i in range(self.height):
             self.maze.append([i*self.width])
             self.maze.append([-1]*(self.width*2-1))
@@ -26,14 +25,15 @@ class lab_gen:
 
         while loop:
             num +=1
-
+            #select a random wall to break
             rand1 = randint(1,self.height*2-3)
             rand2 = randint(1,self.width*2-3)
 
 
             if self.maze[rand1][rand2] == -1 and self.maze[rand1][rand2+1] != -1 and self.maze[rand1][rand2-1] != -1 and self.maze[rand1][rand2+1] != self.maze[rand1][rand2-1]:
+                #break the wall
                 self.maze[rand1][rand2] = -2
-
+                
                 if self.maze[rand1][rand2+1] > self.maze[rand1][rand2-1]:#abracadabra un labyrinthe selmiammaimcestbon
                     self.impact(self.maze[rand1][rand2-1],self.maze[rand1][rand2+1])
 
@@ -43,6 +43,7 @@ class lab_gen:
 
 
             elif self.maze[rand1][rand2] == -1 and self.maze[rand1+1][rand2] != -1 and self.maze[rand1-1][rand2] != -1 and self.maze[rand1+1][rand2] != self.maze[rand1-1][rand2]:
+                #break the wall
                 self.maze[rand1][rand2] = -2
 
                 if self.maze[rand1+1][rand2] > self.maze[rand1-1][rand2]:
@@ -59,34 +60,28 @@ class lab_gen:
         self.const()
 
 
-
+    #change all @old_num by @new_num
     def impact(self,new_num,old_num):
         for i in range(len(self.maze)):
-
             for j in range(len(self.maze[i])):
                 if self.maze[i][j] == old_num:
                     self.maze[i][j] = new_num
 
 
-
+    #transforme the list maze to be understandable by the other program
     def const(self):
         for i in range(len(self.maze)):
             for j in range(len(self.maze[i])):
                 k = self.maze[i][j]
-
                 if k == -1:
-                    self.maze[i][j] = 3
-
-                elif k == -2:
-                    self.maze[i][j] = 0
-
+                    self.maze[i][j] = 2
                 else:
                     self.maze[i][j] = 0
 
-        self.maze = [[3]*(len(self.maze[1]))] + self.maze
+        self.maze = [[2]*(len(self.maze[1]))] + self.maze
 
-        [self.maze[i].insert(0,3) for i in range(len(self.maze))]
-        [self.maze[i].append(3) for i in range(len(self.maze))]
+        [self.maze[i].insert(0,2) for i in range(len(self.maze))]
+        [self.maze[i].append(2) for i in range(len(self.maze))]
 
         self.maze = [self.maze,[0]]
         print(self.maze)
