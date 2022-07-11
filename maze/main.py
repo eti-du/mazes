c8943e15873f26934e23251c282b46ac8c3ec0a0
NBRET-TOUCH-WASH <nbret.touchard.nsi@protonmail.com>
2022-07-11 18:27:47 +0200

    Update main.py
    grasping the code, getting there...

--- maze/main.py
+++ maze/main.py
@@ -49,22 +49,48 @@ class main():
 
 
     #menu creation
-    pygame.draw.rect(window, (70,15,20), [0, 0, window_size[0], window_size[1]],0)#fill the window in red
+    #draws the background (solid color)
+    pygame.draw.rect(window, (70,15,20), [0, 0, window_size[0], window_size[1]],0)
 
-    pygame.draw.rect(window, (40,30,100), [window_size[0]//2-200, window_size[1]*0.65, 400, 70],0)
-    pygame.draw.rect(window, (40,30,100), [window_size[0]//2-200, window_size[1]*0.75, 400, 70],0)
-    pygame.draw.rect(window, (40,30,100), [window_size[0]//2-200, window_size[1]*0.85, 400, 70],0)
+    #buttons backgrounds/boxes
+    pygame.draw.rect(window, (40,30,100), [window_size[0]//2-200, window_size[1]*0.65, 400, 70],0)#"Play" button box
+    pygame.draw.rect(window, (40,30,100), [window_size[0]//2-200, window_size[1]*0.75, 400, 70],0)#maze selection button box
+    pygame.draw.rect(window, (40,30,100), [window_size[0]//2-200, window_size[1]*0.85, 400, 70],0)#maze randomize button box
+
+    #Title
+    window.blit(font.render("Mazes ???",
+                            True,
+                            (85,100,240)),
+                (window_size[0]//2 - 75*5,
+                window_size[1]//4))
+
+    #buttons labels
+    #"Play" button label
+    window.blit(font1.render("Play",
+                            True,
+                            (85,100,240)),
+                (window_size[0]//2 - 60,
+                window_size[1]*0.65 + 10))
+
+    #maze selection button label
+    window.blit(font1.render("Choose maze",
+                            True,
+                            (85,100,240)),
+                (window_size[0]//2 - 170,
+                window_size[1]*0.75 + 10))
+
+    #maze randomizer button label
+    window.blit(font1.render("Random maze",
+                            True,
+                            (85,100,240)),
+                (window_size[0]//2 - 170,
+                window_size[1]*0.85 + 10))
 
 
-    window.blit(font.render("Mazes ???", True, (85,100,240)),(window_size[0]//2-75*5,window_size[1]//4))
-    window.blit(font1.render("Play", True, (85,100,240)),(window_size[0]//2-60,window_size[1]*0.65+10))
-    window.blit(font1.render("Choose maze", True, (85,100,240)),(window_size[0]//2-170,window_size[1]*0.75+10))
-    window.blit(font1.render("Random maze", True, (85,100,240)),(window_size[0]//2-170,window_size[1]*0.85+10))
-
-    pygame.display.update()
+    pygame.display.update()#what does this exactly do?
 
 
-    #wait until the player click on a button 
+    #wait until the player clicks on a button
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
@@ -72,8 +98,17 @@ class main():
                 pygame.quit()
 
             elif event.type == pygame.MOUSEBUTTONUP:
-                #Test if the player clicked on "play"
-                if (window_size[0] // 2 - 200) < event.pos[0] < (window_size[0] // 2 + 400) and (window_size[1] * 0.65) < event.pos[1] < (window_size[1] * 0.65 + 70):
+                """
+                Tests if the player clicked on "Play"
+                µ couldn't we use a simpler measure to get the mouse's position as a tuple?
+                µ e.g. (140,202)
+                
+                µ better yet, couldn't we use some sort of ID for each button,
+                µ to automatically know which button was pressed?
+                µ i think it'd help because right now the conditions are nothing short of a
+                µ nightmare to read and comprehend
+                """
+                if (window_size[0]//2 - 200) < event.pos[0] < (window_size[0]//2 + 400) and (window_size[1]*0.65) < event.pos[1] < (window_size[1]*0.65 + 70):
                     pygame.draw.rect(window, (100,130,140), [0, 0, window_size[0], window_size[1]],0)
                     loop = False
 
@@ -82,16 +117,23 @@ class main():
 
                     lab.main_function(window,window_size, content['maze'], content['init_position'])
 
+
                 #Same for "Choose maze"
-                elif (window_size[0] // 2 - 200) < event.pos[0] < (window_size[0] // 2 + 400) and (window_size[1] * 0.75) < event.pos[1] < (window_size[1] * 0.75 + 70):
+                elif (window_size[0]//2 - 200) < event.pos[0] < (window_size[0]//2 + 400) and (window_size[1]*0.75) < event.pos[1] < (window_size[1]*0.75 + 70):
                     pygame.draw.rect(window, (70,15,20), [0, 0, window_size[0], window_size[1]],0)
                     loop = False
-                    ui_choice.ui_mazechoice(window,window_size)#launch the other menu
+                    ui_choice.ui_mazechoice(window,window_size)#launches the maze selection menu
+
 
                 #Same for "Random maze"
-                elif (window_size[0] // 2 - 200) < event.pos[0] < (window_size[0] // 2 + 400) and (window_size[1] * 0.85) < event.pos[1] < (window_size[1] * 0.85 + 70):
+                elif (window_size[0]//2 - 200) < event.pos[0] < (window_size[0]//2 + 400) and (window_size[1]*0.85) < event.pos[1] < (window_size[1]*0.85 + 70):
                     loop = False
-                    lab.main_function(window, window_size, lab_gen(10,10).maze, {"x":3,"y":3,"z":0})#generate a random maze then launch it
 
+                    #generates a random maze and then launches it
+                    lab.main_function(window, window_size, lab_gen(10,10).maze, {"x":3,"y":3,"z":0})
+
+
+        #µ is this useful? i thought we used fps instead?
+        #µ i might be wrong on this one so don't hesitate to tell me
         cl.tick(60)
     pygame.quit()
